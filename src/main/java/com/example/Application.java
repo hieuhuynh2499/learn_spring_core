58a43d23a89f1bfcbfd0fc60977e789b9cb9371b
hieuhuynh2474 <hieuh5355@gmail.com>
2021-06-03 22:56:02 +0700

    learning_bean

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,19 @@
 package com.example;
 
 import com.example.bean.HelloWorld;
+import com.example.bean.HiXml;
 import com.example.config.AppConfig;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContextXml = new ClassPathXmlApplicationContext("appContextXml.xml");
+        HiXml hiXml = (HiXml) applicationContextXml.getBean("hibean");
         HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloWorldBean");
         System.out.println(helloWorld.getMessage());
+        System.out.println(hiXml.getMessage());
     }
 }
